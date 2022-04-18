4c07280ca5a6d96d3462480071538abb4f793540
otmdgnso <wkdtmdgnsp@gmail.com>
2022-04-18 15:34:36 +0900

    학생 정보 back 추가

--- src/main/java/org/kh/campus/student/controller/StudentController.java
+++ src/main/java/org/kh/campus/student/controller/StudentController.java
@@ -6,7 +6,9 @@ import org.kh.campus.student.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import com.nexacro.uiadapter17.spring.core.annotation.ParamDataSet;
 import com.nexacro.uiadapter17.spring.core.data.NexacroResult;
+import com.nexacro17.xapi.data.DataSet;
 
 @Controller
 public class StudentController {
@@ -30,9 +32,39 @@ public class StudentController {
 	}
 	
 	// 학생 정보 수정
-	public NexacroResult modifyStudent() {
+	public NexacroResult modifyStudent(
+			 @ParamDataSet(name="in_std") 	DataSet inStd
+			) throws Exception {
+		int 	nErrorCode = 0;
+		String  strErrorMsg = "START";
 		NexacroResult result = new NexacroResult();
+		int studentNo = 0;
 		
+		for(int i = 0; i < inStd.getRowCount(); i++) {
+			int rowType = inStd.getRowType(i);
+			String studentPhonenumber = dsGet(inStd, i, "studentPhonenumber");
+			String studentAddress = dsGet(inStd, i, "studentAddress");
+			String studentEmail = dsGet(inStd, i, "studentEmail");
+			Student studnt = new Student(
+					studentPhonenumber,
+					studentAddress,
+					studentEmail
+					);
+			if( rowType == DataSet.ROW_TYPE_INSERTED) {
+				sService.printStudent(studentNo);
+		}
+		}
 		return result;
 	}
+	
+	// Dataset value
+		public String dsGet(DataSet ds, int rowno, String colid) throws Exception
+		{
+		    String value;
+		    value = ds.getString(rowno, colid);
+		    if( value == null )
+		        return "";
+		    else
+		        return value;
+		} 
 }
