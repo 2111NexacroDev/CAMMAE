84fd05ac75400d2c30103865eaf93aed03c7e3eb
SEOYOON <seoy8n@gmail.com>
2022-04-19 09:53:07 +0900

    질의응답게시판 주석추가

--- src/main/java/org/kh/campus/question/controller/QuestionController.java
+++ src/main/java/org/kh/campus/question/controller/QuestionController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class QuestionController {
 	
 
-	
+	//게시글 등록페이지
 	@RequestMapping(value="question/registerView")
 	public String questionWriteView(Model model) {
 		
