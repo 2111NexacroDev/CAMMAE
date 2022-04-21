c8170703fa40584e27bfc20a6e4a6e64c9b88f66
SEOYOON <seoy8n@gmail.com>
2022-04-21 14:31:09 +0900

    질의응답 게시판 페이징처리

--- src/main/java/org/kh/campus/question/store/QuestionStore.java
+++ src/main/java/org/kh/campus/question/store/QuestionStore.java
@@ -3,20 +3,22 @@ package org.kh.campus.question.store;
 import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
+import org.kh.campus.question.domain.PageInfo;
 import org.kh.campus.question.domain.Question;
 import org.kh.campus.question.domain.QuestionReply;
 import org.kh.campus.question.domain.QuestionSearch;
 
 public interface QuestionStore {
 
-	public List<Question> selectAllQuestion(SqlSession sqlSession);
+	public List<Question> selectAllQuestion(SqlSession sqlSession, PageInfo pi);
 	public List<Question> selectSearchQuestion(QuestionSearch questionSearch, SqlSession sqlSession);
 	public Question selectOneQuestion(SqlSession sqlSession, int questionNo);
 	public int insertQuestion(Question question, SqlSession sqlSession);
 	public int updateQuestion(Question question, SqlSession sqlSession);
 	public int updateCount(int questionNo, SqlSession sqlSession);
 	public int deleteQuestion(int questionNo, SqlSession sqlSession);
-	
+	//페이징
+	public int selectListCount(SqlSession sqlSession);
 	//댓글
 	public List<QuestionReply> selectAllReply(int questionNo, SqlSession sqlSession);
 	public int insertReply(QuestionReply questionReply, SqlSession sqlSession);
