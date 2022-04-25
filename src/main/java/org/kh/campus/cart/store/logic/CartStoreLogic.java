1f60703376391af9f840a9c77d4aa468b1f20fd1
user1 <user1@DESKTOP-G4RRIS3>
2022-04-25 17:10:59 +0900

    예비수강신청 등록

--- src/main/java/org/kh/campus/cart/store/logic/CartStoreLogic.java
+++ src/main/java/org/kh/campus/cart/store/logic/CartStoreLogic.java
@@ -5,20 +5,21 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 import org.kh.campus.cart.domain.Cart;
 import org.kh.campus.cart.store.CartStore;
+import org.kh.campus.lecture.domain.Lecture;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CartStoreLogic implements CartStore {
 
 	@Override
-	public List<Cart> selectAllCart(SqlSession sqlSession) {
-		List<Cart> cList = sqlSession.selectList("");
-		return cList;
+	public List<Lecture> selectAllCart(SqlSession sqlSession) {
+		List<Lecture> lList = sqlSession.selectList("CartMapper.selectAllCart");
+		return lList;
 	}
 
 	@Override
-	public int insertCart(SqlSession sqlSession, Cart cart) {
-		int result =  sqlSession.insert("", cart);
+	public int insertCart(SqlSession sqlSession, int lectureNo) {
+		int result = sqlSession.insert("CartMapper.insertCart", lectureNo);
 		return result;
 	}
 
@@ -27,6 +28,9 @@ public class CartStoreLogic implements CartStore {
 		int result = sqlSession.delete("",lectureNo);
 		return result;
 	}
+
+
+
 
 	
 }
