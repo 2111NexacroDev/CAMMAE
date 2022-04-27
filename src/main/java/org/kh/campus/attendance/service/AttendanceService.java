271c73a98f206d1b5601ba6004bdb4689761d968
suyeoun <tndus01078@naver.com>
2022-04-27 10:34:57 +0900

    게시판 수정 jsp

--- src/main/java/org/kh/campus/attendance/service/AttendanceService.java
+++ src/main/java/org/kh/campus/attendance/service/AttendanceService.java
@@ -1,5 +1,22 @@
 package org.kh.campus.attendance.service;
 
+import java.util.List;
+
+import org.kh.campus.attendance.domain.Attendance;
+
 public interface AttendanceService {
+	//학생
+		public List<Attendance> printAllAttendance();//과목출결상태조회
+		//과목조회 변경 셀렉트 박스
+		//이의신청 신청하기 버튼
+		//출결이의신청조회
+		
+		//교수
+		//과목선택셀렉트 박스
+		//날짜선택셀렉트 박스
+		//학생목록조회
+		//상태변경셀렉트 박스
+		//출석이의신청학생리스트 조회
+		//상태변경 박스
 
 }
