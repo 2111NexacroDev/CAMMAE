087113e7a1406af58924396e15da68161075585f
SEOYOON <seoy8n@gmail.com>
2022-05-06 23:20:53 +0900

    tuition 패키지 삭제

--- src/main/java/org/kh/campus/tuition/service/TuitionService.java
+++
@@ -1,5 +0,0 @@
-package org.kh.campus.tuition.service;
-
-public interface TuitionService {
-
-}
--- src/main/java/org/kh/campus/tuition/store/logic/TuitionStoreLogic.java
+++
@@ -1,9 +0,0 @@
-package org.kh.campus.tuition.store.logic;
-
-import org.kh.campus.tuition.store.TuitionStore;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class TuitionStoreLogic implements TuitionStore {
-
-}
