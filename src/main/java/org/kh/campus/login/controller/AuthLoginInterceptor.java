db3bc76e525f0056d6a2e25b2102a5dcd54bd1c7
Hoon <Hoon@61.74.160.147>
2022-05-18 23:59:56 +0900

    nexa test시 불편을 고려하여 공통 로그인 권한 요청 주석처리 했어요 commit

--- src/main/java/org/kh/campus/login/controller/AuthLoginInterceptor.java
+++ src/main/java/org/kh/campus/login/controller/AuthLoginInterceptor.java
@@ -14,15 +14,15 @@ public class AuthLoginInterceptor extends HandlerInterceptorAdapter {
         // session 객체를 가져옴
         HttpSession session = request.getSession();
         // login처리를 담당하는 사용자 정보를 담고 있는 객체를 가져옴
-        Object obj1 = session.getAttribute("loginUser");
-        Object obj2 = session.getAttribute("loginProfessor");
-        Object obj3 = session.getAttribute("loginManager");
+        Object std = session.getAttribute("loginUser");
+        Object prf = session.getAttribute("loginProfessor");
+        Object mag = session.getAttribute("loginManager");
           
-        if ( obj1 == null &&  obj2 == null &&  obj3 == null){
-            // 로그인이 안되어 있는 상태임으로 로그인 폼으로 다시 돌려보냄(redirect)
-            response.sendRedirect("/login/loginPage.kh");
-            return false; // 더이상 컨트롤러 요청으로 가지 않도록 false로 반환함
-        }
+		/*
+		 * if ( std == null && prf == null && mag == null){ // 로그인이 안되어 있는 상태임으로 로그인 폼으로
+		 * 다시 돌려보냄(redirect) response.sendRedirect("/login/loginPage.kh"); return false;
+		 * // 더이상 컨트롤러 요청으로 가지 않도록 false로 반환함 }
+		 */
           
         // preHandle의 return은 컨트롤러 요청 uri로 가도 되냐 안되냐를 허가하는 의미임
         // 따라서 true로하면 컨트롤러 uri로 가게 됨.
